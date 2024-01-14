8dea34dc75d8d9f7476d0d4ff0f5314ece7c80b5
Eric Mika <eric@ericmika.com>
2024-01-14 01:55:03 -0500

    Pass actual tldr file.

--- scripts/export.ts
+++ scripts/export.ts
@@ -1,55 +1,79 @@
 import express from 'express'
+import fs from 'node:fs/promises'
+import os from 'node:os'
 import path from 'node:path'
 import { dirname } from 'node:path'
 import { fileURLToPath } from 'node:url'
 import puppeteer from 'puppeteer'
 import type { CDPSession } from 'puppeteer'
 
-console.log('Running tldraw...')
+type ExportFormats = 'jpeg' | 'json' | 'png' | 'svg' | 'webp'
 
-// Serve local tldraw
-const app = express()
-const port = 3000 // Or any port you prefer
-app.use(express.static(path.join(dirname(fileURLToPath(import.meta.url)), '../dist')))
+async function exportTldr(tldrPath: string, destination: string, format: ExportFormats = 'svg') {
+	const scriptDirectory = dirname(fileURLToPath(import.meta.url))
 
-const server = app.listen(port, () => {
-	console.log(`tldraw running at http://localhost:${port}`)
-})
+	const resolvedTldrPath = path.join(scriptDirectory, tldrPath)
+	console.log(`Loading tldr file "${resolvedTldrPath}"`)
+	const tldrFile = await fs.readFile(resolvedTldrPath, 'utf8')
+	const tldrFileEncoded = encodeURIComponent(tldrFile)
 
-console.log('Running Puppeteer...')
-// Launch Puppeteer and access the Vite-served website
-const browser = await puppeteer.launch({ headless: 'new' })
-const page = await browser.newPage()
+	console.log('Running tldraw...')
 
-const client = await page.target().createCDPSession()
-await client.send('Browser.setDownloadBehavior', {
-	behavior: 'allowAndName',
-	// TODO os.tmpdir()
-	downloadPath: '/Users/mika/Desktop',
-	eventsEnabled: true,
-})
+	// Serve local tldraw
+	const app = express()
+	const port = 3000 // Or any port you prefer
+	app.use(express.static(path.join(scriptDirectory, '../dist')))
 
-async function waitForDownloadCompletion(client: CDPSession) {
-	return new Promise((resolve, reject) => {
-		client.on('Browser.downloadProgress', async (event) => {
-			if (event.state === 'completed') {
-				await browser.close()
-				console.log('Closed Puppeteer')
-				resolve(event.guid)
-			} else if (event.state === 'canceled') {
-				console.error('Export download canceled')
-				await browser.close()
-				reject(new Error('Download was canceled'))
-			}
-		})
+	const server = app.listen(port, () => {
+		console.log(`tldraw running at http://localhost:${port}`)
 	})
+
+	console.log('Running Puppeteer...')
+	// Launch Puppeteer and access the Vite-served website
+	const browser = await puppeteer.launch({ headless: 'new' })
+	const page = await browser.newPage()
+
+	const client = await page.target().createCDPSession()
+	await client.send('Browser.setDownloadBehavior', {
+		behavior: 'allowAndName',
+		downloadPath: os.tmpdir(),
+		eventsEnabled: true,
+	})
+
+	async function waitForDownloadCompletion(client: CDPSession) {
+		return new Promise((resolve, reject) => {
+			client.on('Browser.downloadProgress', async (event) => {
+				if (event.state === 'completed') {
+					await browser.close()
+					console.log('Closed Puppeteer')
+					resolve(event.guid)
+				} else if (event.state === 'canceled') {
+					console.error('Export download canceled')
+					await browser.close()
+					reject(new Error('Download was canceled'))
+				}
+			})
+		})
+	}
+
+	// Navigate to the URL served by Vite, download starts automatically
+	await page.goto(`http://localhost:${port}?data=${tldrFileEncoded}&format=${format}`) // Adjust the URL to match your Vite configuration
+	// TODO types from function
+	const downloadGuid = (await waitForDownloadCompletion(client)) as string
+	console.log(downloadGuid)
+
+	// Get the base filename without the extension
+	// TODO handle file-like destination
+	const originalFilename = path.basename(tldrPath, path.extname(tldrPath))
+	const downloadPath = path.join(os.tmpdir(), downloadGuid)
+	const destinationPath = path.join(destination, `${originalFilename}.${format}`)
+	await fs.rename(downloadPath, destinationPath)
+
+	console.log(`Saved to "${destinationPath}"`)
+
+	// Stop the Express server
+	server.close()
+	console.log('stopped tldraw server')
 }
 
-// Navigate to the URL served by Vite, download starts automatically
-await page.goto(`http://localhost:${port}`) // Adjust the URL to match your Vite configuration
-const downloadGuid = await waitForDownloadCompletion(client)
-console.log(downloadGuid)
-
-// Stop the Express server
-server.close()
-console.log('stopped tldraw server')
+await exportTldr('../box.tldr', '/Users/mika/Desktop', 'svg')
--- src/app.tsx
+++ src/app.tsx
@@ -1,31 +1,45 @@
+/* eslint-disable @typescript-eslint/no-floating-promises */
 /* eslint-disable unicorn/consistent-function-scoping */
 /* eslint-disable @typescript-eslint/naming-convention */
-/* eslint-disable @typescript-eslint/no-unsafe-argument */
-/* eslint-disable @typescript-eslint/no-unsafe-call */
-/* eslint-disable @typescript-eslint/no-unsafe-member-access */
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+
 import { transact } from '@tldraw/editor'
 import { type Editor, Tldraw, exportAs, parseTldrawJsonFile } from '@tldraw/tldraw'
 import '@tldraw/tldraw/tldraw.css'
 
 // TODO wherefrom import?
 type TLExportType = 'jpeg' | 'json' | 'png' | 'svg' | 'webp'
-const exportType: TLExportType = 'svg'
-const tldrFileContent =
-	'{"tldrawFileFormatVersion":1,"schema":{"schemaVersion":1,"storeVersion":4,"recordVersions":{"asset":{"version":1,"subTypeKey":"type","subTypeVersions":{"image":3,"video":3,"bookmark":1}},"camera":{"version":1},"document":{"version":2},"instance":{"version":22},"instance_page_state":{"version":5},"page":{"version":1},"shape":{"version":3,"subTypeKey":"type","subTypeVersions":{"group":0,"text":1,"bookmark":2,"draw":1,"geo":8,"note":5,"line":1,"frame":0,"arrow":2,"highlight":0,"embed":4,"image":3,"video":2}},"instance_presence":{"version":5},"pointer":{"version":1}}},"records":[{"gridSize":10,"name":"","meta":{},"id":"document:document","typeName":"document"},{"id":"pointer:pointer","typeName":"pointer","x":58,"y":40,"lastActivityTimestamp":1705207679062,"meta":{}},{"meta":{},"id":"page:page","name":"Page 1","index":"a1","typeName":"page"},{"x":0,"y":0,"z":1,"meta":{},"id":"camera:page:page","typeName":"camera"},{"editingShapeId":null,"croppingShapeId":null,"selectedShapeIds":[],"hoveredShapeId":null,"erasingShapeIds":[],"hintingShapeIds":[],"focusedGroupId":null,"meta":{},"id":"instance_page_state:page:page","pageId":"page:page","typeName":"instance_page_state"},{"followingUserId":null,"opacityForNextShape":1,"stylesForNextShape":{"tldraw:geo":"rectangle"},"brush":null,"scribbles":[],"cursor":{"type":"default","rotation":0},"isFocusMode":false,"exportBackground":true,"isDebugMode":false,"isToolLocked":false,"screenBounds":{"x":0,"y":0,"w":1280,"h":1312},"zoomBrush":null,"isGridMode":false,"isPenMode":false,"chatMessage":"","isChatting":false,"highlightedUserIds":[],"canMoveCamera":true,"isFocused":true,"devicePixelRatio":2,"isCoarsePointer":false,"isHoveringCanvas":false,"openMenus":["main menu","main menu menu file"],"isChangingStyle":false,"isReadonly":false,"meta":{},"id":"instance:instance","currentPageId":"page:page","typeName":"instance"},{"x":570,"y":459,"rotation":0,"isLocked":false,"opacity":1,"meta":{},"id":"shape:HHhqOB__0sazC33k-7LDA","type":"geo","props":{"w":77,"h":68,"geo":"rectangle","color":"black","labelColor":"black","fill":"none","dash":"draw","size":"m","font":"draw","text":"","align":"middle","verticalAlign":"middle","growY":0,"url":""},"parentId":"page:page","index":"a1","typeName":"shape"}]}'
+
+// TODO post it instead...
+const parseUrlParams = (): {
+	decodedData: string | undefined
+	format: TLExportType | undefined
+} => {
+	const searchParams = new URLSearchParams(window.location.search)
+	const format = searchParams.get('format') as TLExportType | undefined
+	const data = searchParams.get('data')
+
+	// Assuming 'data' is a URL-encoded JSON string
+	const decodedData = data ? decodeURIComponent(data) : undefined
+
+	return { decodedData, format }
+}
 
 export default function App() {
 	const ready = (editor: Editor) => {
 		console.log('tldraw mounted')
-		console.log(editor)
-		parseAndLoadDocument(editor, tldrFileContent)
-		// Declare type TLExportType = 'jpeg' | 'json' | 'png' | 'svg' | 'webp';
+		// Default to svg
+		const { decodedData, format = 'svg' } = parseUrlParams()
+
+		if (!decodedData) {
+			throw new Error('No data provided')
+		}
+
+		parseAndLoadDocument(editor, decodedData)
 
 		// Look for frames?
 		// const shapeIds = editor.getPageShapeIds('page:page')
 		// console.log(`shapeIds: ${shapeIds}`)
-		// TODO
-		exportAs(editor, [], exportType, {}).then(() => {
+		exportAs(editor, [], format, {}).then(() => {
 			console.log('exported data')
 		})
 	}
