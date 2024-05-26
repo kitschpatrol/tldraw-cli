090908cba915cf1ac97609088ce9a89b7753943c
Eric Mika <eric@ericmika.com>
2024-05-26 18:17:13 -0400

    Test: Fix issues with tldr and json snapshot hash stability.

--- test/utilities/file.ts
+++ test/utilities/file.ts
@@ -2,12 +2,33 @@
 import * as cheerio from 'cheerio'
 import { createHash } from 'node:crypto'
 import { existsSync, readFileSync } from 'node:fs'
+import stringify from 'safe-stable-stringify'
 import { expect } from 'vitest'
 
 function getFileHash(filePath: string): string {
 	const fileBuffer = readFileSync(filePath)
 	const hash = createHash('sha256')
 	hash.update(fileBuffer)
+	return hash.digest('hex')
+}
+
+function getStableJsonHash(filePath: string): string {
+	const jsonContent = readFileSync(filePath, { encoding: 'utf8' })
+	const stableJsonString = stringify(JSON.parse(jsonContent))
+
+	if (stableJsonString === undefined) {
+		throw new Error('Could not parse JSON file')
+	}
+
+	// Even then, some of the IDs seem to fluctuate from run to run
+	// This is brittle
+	const ultraStableJsonString = stableJsonString.replace(
+		/:([^\s,.:]{21})"/g,
+		() => `:XXXXXXXXXXXXXXXXXXXXX"`,
+	)
+
+	const hash = createHash('sha256')
+	hash.update(ultraStableJsonString)
 	return hash.digest('hex')
 }
 
@@ -53,6 +74,10 @@ function expectSvgToMatchHash(filePath: string): void {
 	expect(getStableSvgHash(filePath)).matchSnapshot()
 }
 
+function expectJsonToMatchHash(filePath: string): void {
+	expect(getStableJsonHash(filePath)).matchSnapshot()
+}
+
 function expectFileToExist(filePath: string): void {
 	expect(existsSync(filePath))
 }
@@ -68,6 +93,16 @@ export function expectFileToBeValid(filePath: string, extension: string): void {
 	// Also tested jest-image-snapshot, which was interesting, but it's only
 	// compatible with PNGs, and it and pollutes the repo with big files
 	switch (extension) {
+		case 'json': {
+			expectJsonToMatchHash(filePath)
+			break
+		}
+
+		case 'tldr': {
+			expectJsonToMatchHash(filePath)
+			break
+		}
+
 		case 'svg': {
 			expectSvgToMatchHash(filePath)
 			break
