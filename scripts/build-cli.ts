45488dbed94642e1a4cce55b5f014793e489689e
Eric Mika <eric@ericmika.com>
2024-09-28 02:31:34 -0400

    Fix dynamic require error in CLI.

--- scripts/build-cli.ts
+++ scripts/build-cli.ts
@@ -2,6 +2,14 @@ import esbuild from 'esbuild'
 import iifePlugin from './plugins/esbuild-plugin-iife'
 
 await esbuild.build({
+	// Fixes "Dynamic require of "buffer" is not supported" runtime CLI error
+	// https://github.com/evanw/esbuild/issues/1921#issuecomment-1491470829
+	banner: {
+		js: `
+		import { createRequire as topLevelCreateRequire } from 'module';
+		const require = topLevelCreateRequire(import.meta.url);
+		`,
+	},
 	bundle: true,
 	entryPoints: ['src/cli/cli.ts'],
 	external: ['express', 'puppeteer', 'yargs'],
