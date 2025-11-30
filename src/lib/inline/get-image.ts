7913ec1c2edf7c7bbd922c4cb53da73b4bd8f03e
Eric Mika <eric@ericmika.com>
2025-11-29 19:33:21 -0500

    Update dependencies. Tldraw 4.2.0.

--- src/lib/inline/get-image.ts
+++ src/lib/inline/get-image.ts
@@ -21,7 +21,6 @@ import type { Editor } from 'tldraw'
 import { uint8ArrayToBase64 } from 'uint8array-extras'
 
 declare global {
-	// eslint-disable-next-line ts/naming-convention
 	interface Window {
 		editor: Editor
 		getImage: (options: {
