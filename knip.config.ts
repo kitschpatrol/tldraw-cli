264fb3aafda3953440ceceb61d7b20acae6daac1
Eric Mika <eric@ericmika.com>
2025-12-06 17:27:29 -0500

    Update shared config, should fix local builds.

--- knip.config.ts
+++ knip.config.ts
@@ -2,5 +2,5 @@ import { knipConfig } from '@kitschpatrol/knip-config'
 
 export default knipConfig({
 	entry: ['src/tldraw/main.tsx', './test/utilities/file.ts'],
-	ignoreDependencies: ['@fontsource/inter'],
+	ignoreDependencies: ['@fontsource/inter', 'node-addon-api', 'node-gyp'],
 })
